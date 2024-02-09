79f4612eedeac2a7747fec32262729ad746c0c9c
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-09 16:02:22 -0500

    Task 3 done

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,4 +20,13 @@ $(function() {
         }
         $(".amenities h4").text(amenities_text);
     });
+
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === "OK") {
+            $("#api_status").addClass("available");
+        }
+        else {
+            $("#api_status").removeClass("available");
+        }
+      });
 });
