a43acd76f4721da283736489cc594b72c66fb1a0
Radouane Baba <radoine.baba04@gmail.com>
2024-02-09 18:08:58 +0100

    test task2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+$(function() {
+    const amenities = {};
+    $("input[type=checkbox]").on("change", function () {
+        if ($(this).is(":checked")) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        }
+        else {
+            delete amenities[$(this).data('id')];
+        }
+        amenities_text = "";
+        for (const amenity of Object.values(amenities)) {
+            if (text == "") {
+                text += amenity;
+            } else {
+                text += ", " + amenity;
+            }
+        }
+        $(".amenities h4").text(amenities_text);
+    });
+
+});
